d18b53d29c2528b69a84b251320dbb6e6e016022
Mattdias312 <112134994+Mattdias312@users.noreply.github.com>
2024-11-21 13:20:38 -0300

    ajustes merge

--- stray-code-app/src/app/_service/service.component.ts
+++ stray-code-app/src/app/_service/service.component.ts
@@ -5,7 +5,7 @@ import { LoginModel } from '../_models/login/login-model.component';
 import { catchError, map, Observable, of } from 'rxjs';
 import { response } from 'express';
 
-const loginURI:string = "http://localhost:3003"
+const loginURI:string = "http://localhost:3000"
 
 @Injectable({
   providedIn: 'root'
@@ -29,7 +29,7 @@ export class AutorizacaoService {
 
   cadastrar(usuario: LoginModel): Observable<boolean> {
     if (this.isBrowser) {
-      return this.http.post<any>(`${loginURI}/create`, usuario).pipe(
+      return this.http.post<any>(`${loginURI}/register`, usuario).pipe(
         map((response) => response.success || false),
         catchError((err) => {
           console.error("Erro ao realizar login:", err);
@@ -48,7 +48,7 @@ export class AutorizacaoService {
         'Content-Type': 'application/json',
         'x-access-token': token
       });
-      console.log(`${loginURI}/login/${id}`)
+      console.log(`${loginURI}/user/${id}`)
       console.log('Headers enviados:', headers);
       console.log('Headers enviados (chaves):', headers.keys());
       console.log('Content-Type:', headers.get('Content-Type'));
--- stray-code-back/src/server.js
+++ stray-code-back/src/server.js
@@ -5,31 +5,31 @@ const loginRoute = require('./routes/loginRoute');
 const userRoute = require('./routes/userRoute')
 const questionarioRoute = require('./routes/questionarioRoute');
 const mongoose = require('mongoose');
+const port = 3000;
 const dotenv = require('dotenv');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger_output.json');
+const cors = require('cors');
 
 dotenv.config();
 
-const { MONGO_URI, MONGO_URI_LOCAL } = process.env;
+const {MONGO_URI, MONGO_URI_LOCAL} = process.env
 
-let url = MONGO_URI_LOCAL;
+let url = MONGO_URI_LOCAL ;
+  
 let mongodb = MONGO_URI_LOCAL || url;
-
-mongoose.connect(mongodb, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
+mongoose.connect(mongodb);
 mongoose.Promise = global.Promise;
 let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Erro ao conectar com a base de dados'));
+db.on('error',console.error.bind(console, 'error ao conectar com a base de dados'));
 
+app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extendde: false }));
-app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(bodyParser.urlencoded({extendde: false}));
+app.use('api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(loginRoute);
 app.use(userRoute);
 app.use(questionarioRoute);
+
 
 module.exports = app; 
