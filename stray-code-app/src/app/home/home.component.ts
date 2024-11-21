cd5478bad22937f8e84a0d53150885e89bdab238
Mattdias312 <112134994+Mattdias312@users.noreply.github.com>
2024-11-21 09:35:45 -0300

    console.log

--- stray-code-app/src/app/home/home.component.ts
+++ stray-code-app/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '../material.module';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +10,11 @@ import { MaterialModule } from '../material.module';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  constructor(private cookieService: CookieService) {}
 
+
+  ngOnInit(){
+    console.log("cookie ID",this.cookieService.get('id'));
+    console.log("cookie token",this.cookieService.get('token'));
+  }
 }
